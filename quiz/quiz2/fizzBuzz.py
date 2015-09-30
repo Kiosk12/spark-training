3642e59b341abd5946d1e3dc99eddfbd30df9943
Omnia Gamal El-Din <omnia.gamal1988@gmail.com>
2015-09-30 22:28:44 +0200

    fix

--- quiz/quiz2/fizzBuzz.py
+++ quiz/quiz2/fizzBuzz.py
@@ -1,10 +1,16 @@
 def fizzBuzz (x):
 	remainder_3 = x % 3
 	remainder_5 = x % 5 
-	if (remainder_3 == 0 and remainder_5 == 0) : print("FizzBuzz")
-	elif (remainder_3 == 0) : print("Fizz")
-	elif (remainder_5 == 0) : print("Buzz")
-	else : print(`x`)
+	if (remainder_3 == 0 and remainder_5 == 0): 
+		return "FizzBuzz"
+	elif (remainder_3 == 0): 
+		return "Fizz"
+	elif (remainder_5 == 0): 
+		return "Buzz"
+	else: 
+		return `x`
 
 
-map(fizzBuzz, range(1, 101))
+
+for num in range(1, 101):
+	print(fizzBuzz(num))
