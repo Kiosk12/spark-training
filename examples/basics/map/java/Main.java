b127bcdc2d35babe89275fef5b8207b38c73f2ee
Tamer Mohammed Abdul-Radi <tamerr@cakesolutions.net>
2015-10-06 01:32:40 +0200

    Updates Java examples

--- examples/basics/map/java/Main.java
+++ examples/basics/map/java/Main.java
@@ -2,10 +2,11 @@ import java.util.*;
 import org.apache.spark.api.java.*;
 import org.apache.spark.*;
 
-class Main {
+public class Main {
   public static void main(String[] args) {
-    JavaSparkContext sc = new JavaSparkContext("local[*]", "test");
+    SparkConf conf = new SparkConf().setAppName("test").setMaster("local");
+    JavaSparkContext sc = new JavaSparkContext(conf);
 
-    
+
   }
 }
--- templates/java/Main.java
+++ templates/java/Main.java
@@ -2,9 +2,10 @@ import java.util.*;
 import org.apache.spark.api.java.*;
 import org.apache.spark.*;
 
-class Main {
+public class Main {
   public static void main(String[] args) {
-    JavaSparkContext sc = new JavaSparkContext("local[*]", "test");
+    SparkConf conf = new SparkConf().setAppName("test").setMaster("local");
+    JavaSparkContext sc = new JavaSparkContext(conf);
 
     List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
     int result = sc
