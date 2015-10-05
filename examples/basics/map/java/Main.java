0c1a33453a4e13d76ea63fb78a2cc8bc86dd446a
Tamer Mohammed Abdul-Radi <tamerr@cakesolutions.net>
2015-10-06 01:15:05 +0200

    Adds palceholder for first example

---
+++ examples/basics/map/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import org.apache.spark.api.java.*;
+import org.apache.spark.*;
+
+class Main {
+  public static void main(String[] args) {
+    JavaSparkContext sc = new JavaSparkContext("local[*]", "test");
+
+    
+  }
+}
